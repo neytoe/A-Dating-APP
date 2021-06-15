38d4e946081e68cca3b4ff28dfb86dc4a60f4c3c
neytoe <nanyankah@gmail.com>
2021-06-15 20:20:14 +0100

    add observable

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { AccountsService } from './_services/accounts.service';
+import { User } from './_models/user';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,12 +12,19 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor (private http : HttpClient){}
+  constructor (private http : HttpClient, private accountService: AccountsService){}
 
     ngOnInit(){
       this.getUsers();
+      this.setCurrentUser();
     }
 
+    setCurrentUser(){
+      const user: User = JSON.parse(localStorage.getItem('user'));
+      this.accountService.setCurrentUser(user);
+    }
+
+    //httpcall 
     getUsers(){
       this.http.get('https://localhost:5001/api/users/GetAllUsers')
       .subscribe(response =>{
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,25 +1,29 @@
 import { logging } from 'protractor';
 import { AccountsService } from './../_services/accounts.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../_models/user';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
+
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn: boolean ;
+  currentUser$: Observable<User>;
+
+
   constructor(private accountsService: AccountsService) { }
 
   ngOnInit(): void {
+    this.currentUser$ = this.accountsService.currentUser$;
   }
 
   login() {
     this.accountsService.login(this.model).subscribe( response => {
       console.log(response);
-      this.loggedIn = true;
-      console.log(this.loggedIn);
     }, error => {
       console.log(error);
     });
@@ -27,6 +31,8 @@ export class NavComponent implements OnInit {
 
   logout()
   {
-    this.loggedIn = false;
+    this.accountsService.logout();
   }
+
+
 }
