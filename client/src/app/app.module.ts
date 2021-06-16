1d613a997bd46210332672549452d277bb31a37d
neytoe <nanyankah@gmail.com>
2021-06-16 23:09:18 +0100

    add register page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,12 +10,14 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
